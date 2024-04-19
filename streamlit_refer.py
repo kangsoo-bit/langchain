f5e2430f2200b59cb2591a2789b74b5566bbfb8b
kangsookim <boningskim@gmail.com>
2024-04-19 17:09:23 +0900

    v0.1.12

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -28,7 +28,7 @@ def main():
     page_icon=":books:")
 
     st.title("_Osstem [MPMS연구소] :red[QA Chat]_ :books:")
-    st.title("Model: gpt-4-turbo(2024-04-19) v0.1.11")
+    st.title("Model: gpt-4-turbo(2024-04-19) v0.1.12")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -124,7 +124,7 @@ def get_text(docs):
         elif '.xlsx' in doc.name:
             df = pd.read_excel(file_name, sheet_name='통합고시', header=0)
             #loader = UnstructuredExcelLoader(file_name, mode="elements")
-            loader = DataFrameLoader(df, page_content_column="주요내용")
+            loader = DataFrameLoader(df, page_content_column="주요내용 & 고시 개정내용")
             documents = loader.load_and_split()
 
         doc_list.extend(documents)
